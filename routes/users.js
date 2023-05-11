df5f7c0abd0364a0d73d2f68b3af5b245f67d406
尔尔辞晚 <2673670601@qq.com>
2023-05-11 08:51:19 +0800

    添加前端资源

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.post('/login', (req, res) => {
       }, SECRET, {
         expiresIn: 60 * 60
       })
-      send(res, '0000', '登录成功',{token})
+      send(res, '0000', '登录成功', { token })
       return;
     }
     send(res, '0001', '用户名或密码错误')
@@ -29,11 +29,14 @@ router.post('/login', (req, res) => {
 router.post('/register', (req, res) => {
   const { username, password } = req.body
   UserModel.create({ username, password: md5(password) }).then((data) => {
-    send(res, '0000', '注册成功',{ data })
+    send(res, '0000', '恭喜,注册成功!', { data })
   }, err => send(res, '0001', '该用户已存在，注册失败'))
 })
 router.get('/test', checkTokenMidware, (req, res) => {
-  send(res, '0000', 'token正确', req.user )
+  send(res, '0000', 'token正确', req.user)
 })
 
+router.get('/', (req, res) => {
+  send(res, '0000', '哈哈哈哈哈')
+})
 module.exports = router;
